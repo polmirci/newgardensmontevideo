f66e3eb2f4f70cf6c300600bca0bdc77bb402279
polmirci <polmirci@gmail.com>
2026-02-09 19:44:01 -0300

    feat: Update dependencies and image sources
    Updates project dependencies to their latest stable versions and replaces hardcoded Facebook CDN image URLs with Unsplash links for improved reliability and visual appeal. Also refactors vite configuration to a simpler setup.

--- components/About.tsx
+++ components/About.tsx
@@ -17,13 +17,13 @@ const About: React.FC = () => {
                 <div className="grid grid-cols-2 gap-4">
                     {/* Image 1: Round Topiary Bush / Manicured Garden */}
                     <img 
-                        src="https://scontent.fmvd4-1.fna.fbcdn.net/v/t1.6435-9/81529155_680925628980833_1252516814039023616_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=833d8c&_nc_ohc=bU6JmK6pkqIQ7kNvwHO-4Z7&_nc_oc=AdkDxmx1T4PBr0Wmv9pqRnxLvN6ZootHhhPfBosH5IZkeJ5D6wZcQsn_DKsChlrv4TzJrBJ_GeLdvTk1ejAG15sp&_nc_zt=23&_nc_ht=scontent.fmvd4-1.fna&_nc_gid=9g1QfeYBYAzNZTX0qCCcdA&oh=00_Afvr9rq7Ghg9qLDofxIOC-7Dxrx6jAr9ySpYSR0NPfefxw&oe=69B1B5F0" 
+                        src="https://images.unsplash.com/photo-1610309650392-411a0c710486?auto=format&fit=crop&q=80&w=800" 
                         alt="Poda de arbustos y toparios" 
                         className="w-full h-64 object-cover rounded-2xl shadow-lg mt-8"
                     />
                     {/* Image 2: Lush Green Vegetation */}
                     <img 
-                        src="https://scontent.fmvd1-1.fna.fbcdn.net/v/t1.6435-9/98119357_772893236450738_5353375670141452288_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=0b6b33&_nc_ohc=QcMoIGKxPVYQ7kNvwEdkIWz&_nc_oc=Adl0sipJ3eAxmVduiAWZv6fOM5yrl2B5RCk_efWDb2u14Bh5MkSLPHxCgpKoubOlSKC7nij4TumZsu6APBt560XN&_nc_zt=23&_nc_ht=scontent.fmvd1-1.fna&_nc_gid=GqO2P1HUx_anFUzAKL-Jsg&oh=00_Aftfegj1xvmyc5i_srCLQuXQF-hyBf_7EQLZXD65pUNZHA&oe=69B1B21E" 
+                        src="https://images.unsplash.com/photo-1557429287-b2e26467fc2b?auto=format&fit=crop&q=80&w=800" 
                         alt="Mantenimiento de jardines y cercos" 
                         className="w-full h-64 object-cover rounded-2xl shadow-lg"
                     />
